0a74d5572c09df65d967919a5dbde573c746fb0d
Thorsten Ludewig <th@l9g.de>
2018-03-08 21:00:35 +0100

    application config in a seperate file

---
+++ lib/private/ApplicationConfig.h
@@ -0,0 +1,8 @@
+#ifndef __APPLICATION_CONFIG_H__
+#define __APPLICATION_CONFIG_H__
+
+#define APP_NAME "WiFi OpenHAB Socket"
+#define APP_VERSION "2.0.6"
+#define APP_AUTHOR "Dr. Thorsten Ludewig <t.ludewig@gmail.com>"
+
+#endif
